a8daba784f13ffcca123041cc4048addcad89b93
Angelina <gelka.brl@mail.ru>
2022-06-05 01:47:04 +0300

    метод addAll

--- src/SerchMin.java
+++ src/SerchMin.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class SerchMin {
 
     public static void main(String[] args) {
+//        List<Integer> array = Arrays.asList(10, 15, 25, 2, 8);
         List<Integer> numbers = new ArrayList<>();
+//        numbers.addAll(array);
+//        System.out.println(numbers);
         Random generator = new Random();
         for (int i = 0; i < 20; i++) {
             numbers.add(i, generator.nextInt(15) + 5);
